7d31043dafd4d78c1162d0f2360d9e36a93f0fc9
Roy He <royhe62@yahoo.ca>
2020-10-01 20:27:04 -0700

    Installed all modules

---
+++ testing.js
@@ -0,0 +1,25 @@
+//npm install express mysql cors aes256
+
+
+var aes256 = require('aes256');
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const {PORT = 4000} = process.env
+const mysql = require('mysql');
+ 
+var key = 'asdfl;sdjlkfjlk;asdlkj';
+var plaintext = 'my plaintext message';
+ 
+var encrypted = aes256.encrypt(key, plaintext);
+var decrypted = aes256.decrypt(key, encrypted);
+
+console.log(decrypted)
+
+app.get('/', (req, res) =>{
+    res.end("got to /login for login info")
+  })
+
+app.listen(PORT, () => {
+console.log('Server Loaded on port ${PORT}');
+})
