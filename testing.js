6445de6c22507c03d2225eb284b106d7ec8ddd8a
Roy He <royhe62@yahoo.ca>
2020-10-02 20:37:40 -0700

    added salted hashes for signup page

--- testing.js
+++ testing.js
@@ -8,6 +8,8 @@ const app = express();
 const {PORT = 4000} = process.env
 const mysql = require('mysql');
 const Key = require('./key');
+const signUPSalt = require('node-forge');
+const signUpPass = require('node-forge');
  
 //var key = 'asdfl;sdjlkfjlk;asdlkj';
 //var plaintext = 'my plaintext message';
@@ -15,13 +17,30 @@ const Key = require('./key');
 //var encrypted = aes256.encrypt(key, plaintext);
 //var decrypted = aes256.decrypt(key, encrypted);
 
-console.log(Key.getKey("Hello", "General"));
-console.log(Key.getKey("Hello", "Monkeys"));
-
 app.get('/', (req, res) =>{
     res.end("got to /login for login info")
   })
 
+app.get('/signup', (req, res) =>{
+  const{username, password} = req.query;
+  let saltGenerator = signUPSalt.md.sha256.create();
+  let passHashGenerator = signUpPass.md.sha256.create();
+  let saltedPassword;
+  let passHash;
+
+  saltGenerator.update(username.substr(username.length/2));
+
+  saltedPassword = password + saltGenerator.digest().toHex();
+  console.log(saltedPassword);
+
+  passHashGenerator.update(saltedPassword);
+
+  passHash = passHashGenerator.digest().toHex();
+  console.log(passHash);
+
+  res.end("signup page");
+})
+
 app.listen(PORT, () => {
-console.log('Server Loaded on port ${PORT}');
+  console.log('Server Loaded on port ${PORT}');
 })
